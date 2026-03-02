4a8f7d3ede8cbff2e5bfaae625302e361436d546
CarlosLauter <toninhoyuki@gmail.com>
2026-03-01 22:11:35 -0300

    Adicionando comentários

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -2,7 +2,10 @@
 
 main(){
 
-  char estado1, estado2, codigo_carta1[10], codigo_carta2[10], nome_cidade1[20], nome_cidade2[20];
+  //Não usei nenhum acento ou ç, pois aparecia como símbolos estranhos no console
+
+
+  char estado1, estado2, codigo_carta1[10], codigo_carta2[10], nome_cidade1[20], nome_cidade2[20]; //declarando todas as variáveis 
   int populacao1, populacao2, num_pontos_turis1, num_pontos_turis2;
   float area_km2_1, area_km2_2, pib1, pib2;
 
@@ -10,14 +13,14 @@ main(){
 
   printf("\n\nPrimeira carta:\n");
   printf("\nDigite a letra do Estado: ");
-  scanf(" %c", &estado1);
-  getchar();
+  scanf(" %c", &estado1); //usei o espaço antes do %c como dito em aula para não dar problema de buffer
+  getchar(); //estava tendo problemas, pulava as minhas perguntas, pesquisei na internet e manda usar isso e resolveu, aparentemente limpa o buffer
   printf("\nDigite o codigo da carta, composto pela letra do estado seguido de um numero de 01 a 04: ");
   scanf("%s", codigo_carta1);
-  getchar();
+  getchar(); //estava tendo problemas, pulava as minhas perguntas, pesquisei na internet e manda usar isso e resolveu, aparentemente limpa o buffer
   printf("\nDigite o nome da cidade: ");
-  fgets(nome_cidade1, 20, stdin);
-  nome_cidade1[strcspn(nome_cidade1, "\n")] = 0;
+  fgets(nome_cidade1, 20, stdin); //usei fgets pelo nome da cidade poder ter espaços, conforme exemplo São Paulo
+  nome_cidade1[strcspn(nome_cidade1, "\n")] = 0; //usado conforme mostrado no texto da aula
   printf("\nDigite a populacao da cidade escolhida: ");
   scanf("%d", &populacao1);
   printf("\nDigite a area em km2 da cidade escolhida: ");
@@ -27,18 +30,18 @@ main(){
   printf("\nDigite o numero de pontos turisticos da cidade escolhida: ");
   scanf("%d", &num_pontos_turis1);
 
-  printf("\n\n");
+  printf("\n\n"); //separar e ficar mais legível tanto no console quanto no código
 
   printf("Segunda carta:\n");
   printf("\nDigite a letra do Estado: ");
-  scanf(" %c", &estado2);
-  getchar();
+  scanf(" %c", &estado2); //usei o espaço antes do %c como dito em aula para não dar problema de buffer
+  getchar(); //estava tendo problemas, pulava as minhas perguntas, pesquisei na internet e manda usar isso e resolveu, aparentemente limpa o buffer
   printf("\nDigite o codigo da carta, composto pela letra do estado seguido de um numero de 01 a 04: ");
   scanf("%s", codigo_carta2);
-  getchar();
+  getchar(); //estava tendo problemas, pulava as minhas perguntas, pesquisei na internet e manda usar isso e resolveu, aparentemente limpa o buffer
   printf("\nDigite o nome da cidade: ");
-  fgets(nome_cidade2, 20, stdin);
-  nome_cidade2[strcspn(nome_cidade2, "\n")] = 0;
+  fgets(nome_cidade2, 20, stdin); //usei fgets pelo nome da cidade poder ter espaços, conforme exemplo Rio de janeiro
+  nome_cidade2[strcspn(nome_cidade2, "\n")] = 0; //usado conforme mostrado no texto da aula
   printf("\nDigite a populacao da cidade escolhida: ");
   scanf("%d", &populacao2);
   printf("\nDigite a area em km2 da cidade escolhida: ");
@@ -48,15 +51,15 @@ main(){
   printf("\nDigite o numero de pontos turisticos da cidade escolhida: ");
   scanf("%d", &num_pontos_turis2);
 
-  printf("\n\n");
+  printf("\n\n"); //separar e ficar mais legível tanto no console quanto no código
 
   printf("\nCarta 1:");
   printf("\nEstado: %c", estado1);
   printf("\nCodigo: %s", codigo_carta1);
   printf("\nNome da Cidade: %s", nome_cidade1);
   printf("\nPopulacao: %d", populacao1);
-  printf("\nArea: %.2f km2", area_km2_1);
-  printf("\nPIB: %.2f bilhoes de reais", pib1);
+  printf("\nArea: %.2f km2", area_km2_1); //usei .2f para permitir apenas 2 número após a "vírgula", conforme exemplo
+  printf("\nPIB: %.2f bilhoes de reais", pib1); //usei .2f para permitir apenas 2 número após a "vírgula", conforme exemplo
   printf("\nNumero de Pontos Turisticos: %d", num_pontos_turis1);
   
   printf("\n");
@@ -66,8 +69,8 @@ main(){
   printf("\nCodigo: %s", codigo_carta2);
   printf("\nNome da Cidade: %s", nome_cidade2);
   printf("\nPopulacao: %d", populacao2);
-  printf("\nArea: %.2f km2", area_km2_2);
-  printf("\nPIB: %.2f bilhoes de reais", pib2);
+  printf("\nArea: %.2f km2", area_km2_2); //usei .2f para permitir apenas 2 número após a "vírgula", conforme exemplo
+  printf("\nPIB: %.2f bilhoes de reais", pib2); //usei .2f para permitir apenas 2 número após a "vírgula", conforme exemplo
   printf("\nNumero de Pontos Turisticos: %d", num_pontos_turis2);
 
 
